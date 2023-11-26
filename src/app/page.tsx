5301726a2f6fa088013cf96d952d4f7e60347ece
Ibrahim Aji <ibrahim.aji@live.com>
2023-11-26 10:33:01 +0700

    feat: auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+'use client';
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
-  return <div className="border-1 bg-rose-700 font-bold text-red-400 " />;
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <div>
+      {session ? (
+        <div className="flex justify-between p-8">
+          <div className="font-bold">App.</div>
+          <div className="flex items-center gap-4">
+            <div>{session.user?.name}</div>
+            <img alt="Profile Photos" src={session.user?.image} width={40} height={40} className="rounded-full" />
+            <Button onClick={() => signOut()}>Logout</Button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-end p-8">
+          <Button onClick={() => signIn()}>Login</Button>
+        </div>
+      )}
+    </div>
+  );
 }
